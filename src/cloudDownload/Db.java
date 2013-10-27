22529730e5d8ae484d0a0349399154dd81d7f2e1
Carl Xu <xudifsd@gmail.com>
2013-10-27 05:36:16 +0000

    added many Db utils

--- src/cloudDownload/Db.java
+++ src/cloudDownload/Db.java
@@ -3,37 +3,165 @@ package cloudDownload;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Db {
-	private final static String url = "jdbc:mysql://localhost:3306/cloudDownload";
+	private final static String initUrl = "jdbc:mysql://localhost:3306";
+	private final static String dbName = "cloudDownload";
+	private final static String dbUrl = initUrl + "/" + dbName;
+	private final static String tableName = "tasks";
 	private final static String user = "root";
 	private final static String password = "root";
+	private final static String createDb = "CREATE DATABASE IF NOT EXISTS " + dbName + ";";
+	private final static String useDb = "USE " + dbName + ";";
+	private final static String createTable =
+			"CREATE TABLE IF NOT EXISTS " + tableName + " ("
+			+ "id INT NOT NULL AUTO_INCREMENT,"
+			+ "url VARCHAR(200) NOT NULL,"
+			+ "prograss INT(2) NOT NULL DEFAULT '0',"// 0~99
+			+ "state ENUM('pending', 'downloading', 'succeeded', 'failed') DEFAULT 'pending',"
+			+ "size BIGINT DEFAULT '0',"
+			+ "hit INT DEFAULT '0',"
+			+ "retrieveURL VARCHAR(40) DEFAULT '',"// should only contains filename
+			+ "PRIMARY KEY (id)" + ");";
 
-	public static void main(String[] args) {
-		Connection con = null;
-		PreparedStatement pst = null;
+	public static class Task {
+		public int id;
+		public String url;
+		public int prograss;
+		public String state;
+		public long size;
+		public int hit;
+		public String retrieveURL;
 
-		try {
-			String name = "xudifsd";
-			con = DriverManager.getConnection(url, user, password);
+		public Task(int id) throws SQLException {
+			Connection con = DriverManager.getConnection(dbUrl, user, password);
 
-			pst = con.prepareStatement("INSERT INTO user(name) VALUES(?)");
-			pst.setString(1, name);
-			pst.executeUpdate();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		} finally {
-			try {
-				if (pst != null) {
-					pst.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException ex) {
-				ex.printStackTrace();
+			PreparedStatement pst = con.prepareStatement("SELECT * FROM " + tableName + " WHERE id = ?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+
+			if (rs.next()) {
+				this.id = id;
+				this.url = rs.getString("url");
+				this.prograss = rs.getInt("prograss");
+				this.state = rs.getString("state");
+				this.size = rs.getLong("size");
+				this.hit = rs.getInt("hit");
+				this.retrieveURL = rs.getString("retrieveURL");
+			} else
+				throw new SQLException("no task have id " + id);
+		}
+	}
+
+	private enum State {pending, downloading, succeeded, failed};
+
+	public static void initDb() throws SQLException {
+		Connection con = DriverManager.getConnection(initUrl, user, password);
+		Statement stm = con.createStatement();
+		stm.addBatch(createDb);
+		stm.addBatch(useDb);
+		stm.addBatch(createTable);
+		stm.executeBatch();
+	}
+
+	public static Task getTask(int id) throws SQLException {
+		return new Task(id);
+	}
+
+	public static synchronized int newTask(String taskUrl) throws SQLException {
+		taskUrl = taskUrl.trim();
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		while (true) {
+			// this is very inefficient, but very simple to understand
+			PreparedStatement pst = con.prepareStatement("SELECT id FROM " + tableName + " WHERE url = ?");
+			pst.setString(1, taskUrl);
+			ResultSet rs = pst.executeQuery();
+
+			if (rs.next())
+				return rs.getInt("id");
+			else {
+				PreparedStatement insert = con.prepareStatement("INSERT INTO " + tableName + "(url) VALUES(?)");
+				insert.setString(1, taskUrl);
+				insert.executeUpdate();
+				continue;
 			}
 		}
+	}
+
+	public static void updatePrograss(int id, int prograss) throws SQLException {
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		PreparedStatement pst = con.prepareStatement("UPDATE " + tableName + " SET prograss = ? where id = ?");
+		pst.setInt(1, prograss);
+		pst.setInt(2, id);
+		pst.executeUpdate();
+	}
+
+	public static void changeState(int id, State state) throws SQLException {
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		PreparedStatement pst = con.prepareStatement("UPDATE " + tableName + " SET state = ? where id = ?");
+		pst.setString(1, state.toString());
+		pst.setInt(2, id);
+		pst.executeUpdate();
+	}
+
+	public static void startDownload(int id) throws SQLException {
+		changeState(id, State.downloading);
+	}
+
+	public static void finishDownload(int id, String retrieveUrl) throws SQLException {
+		changeState(id, State.succeeded);
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		PreparedStatement pst = con.prepareStatement("UPDATE " + tableName + " SET retrieveURL = ? where id = ?");
+		pst.setString(1, retrieveUrl);
+		pst.setInt(2, id);
+		pst.executeUpdate();
+	}
+
+	public static String retrieve(int id) throws SQLException {
+		// update hit and get retrieveURL
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		PreparedStatement pst = con.prepareStatement("UPDATE " + tableName + " SET hit = hit + 1 where id = ?");
+		pst.setInt(1, id);
+		pst.executeUpdate();
+
+		pst = con.prepareStatement("SELECT retrieveURL FROM " + tableName + " WHERE id = ?");
+		pst.setInt(1, id);
+		ResultSet rs = pst.executeQuery();
+
+		if (rs.next())
+			return rs.getString("retrieveURL");
+		else
+			return null;
+	}
+
+	public static void penalizeHit() throws SQLException {
+		// minus hit of all tasks
+		Connection con = DriverManager.getConnection(dbUrl, user, password);
+
+		PreparedStatement pst = con.prepareStatement("UPDATE " + tableName + " SET hit = hit - 1 where hit > 0");
+		pst.executeUpdate();
+	}
+
+	public static void main(String[] args) throws SQLException {
+		State state = null;
+		if (args[1].equals("pending"))
+			state = State.pending;
+		else if (args[1].equals("downloading"))
+			state = State.downloading;
+		else if (args[1].equals("succeeded"))
+			state = State.succeeded;
+		else if (args[1].equals("failed"))
+			state = State.failed;
+		else
+			System.err.println("error");
+		changeState(Integer.valueOf(args[0]), state);
 	}
 }
